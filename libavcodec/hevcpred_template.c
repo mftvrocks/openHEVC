f29a01252d30f30b73bd0627875e111fe0f76fa8
plepere <pierre-edouard.lepere@insa-rennes.fr>
2013-06-25 17:30:22 +0200

    pred_planar_0 optimisation

--- libavcodec/hevcpred_template.c
+++ libavcodec/hevcpred_template.c
@@ -249,7 +249,7 @@ static void FUNCC(pred_planar_0)(uint8_t *_src, const uint8_t *_top, const uint8
     pixel *src = (pixel*)_src;
     const pixel *top = (const pixel*)_top;
     const pixel *left = (const pixel*)_left;
-    __m128i ly, t0, tx, l0, add, c0,c1,c2,c3, ly1, tmp1, tmp2, mask;
+    __m128i ly, t0, tx, l0, add, c0,c1,c2,c3, ly1, tmp1, tmp2, mask, C0,C2,C3;
     t0= _mm_set1_epi16(top[4]);
     l0= _mm_set1_epi16(left[4]);
     add= _mm_set1_epi16(4);
@@ -257,83 +257,51 @@ static void FUNCC(pred_planar_0)(uint8_t *_src, const uint8_t *_top, const uint8
 	ly= _mm_loadu_si128((__m128i*)left);			//get 16 values
 	ly= _mm_unpacklo_epi8(ly,_mm_setzero_si128());	//drop to 8 values 16 bit
 
-	tx= _mm_loadu_si128((__m128i*)top);				//get 16 values
+	tx= _mm_loadu_si128((__m128i*)top);				//get 16 value
 	tx= _mm_unpacklo_epi8(tx,_mm_setzero_si128());	//drop to 8 values 16 bit
-	tmp1= _mm_set_epi16(0,0,0,0,0,1,2,3);
-	tmp2= _mm_set_epi16(0,0,0,0,4,3,2,1);
+	tx= _mm_unpacklo_epi64(tx,tx);
+	tmp1= _mm_set_epi16(0,1,2,3,0,1,2,3);
 	mask= _mm_set_epi8(0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1);
 
-    	ly1= _mm_set1_epi16(_mm_extract_epi16(ly,0));
+    	ly1= _mm_unpacklo_epi64(_mm_set1_epi16(_mm_extract_epi16(ly,0)),_mm_set1_epi16(_mm_extract_epi16(ly,1)));
+    	ly= _mm_unpacklo_epi64(_mm_set1_epi16(_mm_extract_epi16(ly,2)),_mm_set1_epi16(_mm_extract_epi16(ly,3)));
 
     	c0= _mm_mullo_epi16(tmp1,ly1);
-    	c1= _mm_mullo_epi16(tmp2,t0);
-    	c2= _mm_mullo_epi16(_mm_set1_epi16(3),tx);
-    	c3= _mm_mullo_epi16(_mm_set1_epi16(1),l0);
+    	C0= _mm_mullo_epi16(tmp1,ly);
+    	c1= _mm_mullo_epi16(_mm_set_epi16(4,3,2,1,4,3,2,1),t0);
+    	c2= _mm_mullo_epi16(_mm_set_epi16(2,2,2,2,3,3,3,3),tx);
+
+    	C2= _mm_mullo_epi16(_mm_set_epi16(0,0,0,0,1,1,1,1),tx);
+    	c3= _mm_mullo_epi16(_mm_set_epi16(2,2,2,2,1,1,1,1),l0);
+    	C3= _mm_mullo_epi16(_mm_set_epi16(4,4,4,4,3,3,3,3),l0);
 
     	c0= _mm_add_epi16(c0,c1);
     	c2= _mm_add_epi16(c2,c3);
+    	C2= _mm_add_epi16(C2,C3);
+    	C0= _mm_add_epi16(C0,c1);
     	c2= _mm_add_epi16(c2,add);
+    	C2= _mm_add_epi16(C2,add);
     	c0= _mm_add_epi16(c0,c2);
+    	C0= _mm_add_epi16(C0,C2);
 
     	c0= _mm_srli_epi16(c0,3);
+    	C0= _mm_srli_epi16(C0,3);
 
-    	c0= _mm_packus_epi16(c0,_mm_setzero_si128());
+    	c0= _mm_packus_epi16(c0,C0);
 
     	_mm_maskmoveu_si128(c0,mask,(__m128i*)(src)); //store only 4 values
+    	c0 = _mm_srli_si128(c0,4);
 
-    	ly1= _mm_set1_epi16(_mm_extract_epi16(ly,1));
+    	_mm_maskmoveu_si128(c0,mask,(__m128i*)(src + stride)); //store only 4 values
 
-    	c0= _mm_mullo_epi16(tmp1,ly1);
-    	c1= _mm_mullo_epi16(tmp2,t0);
-    	c2= _mm_mullo_epi16(_mm_set1_epi16(2),tx);
-    	c3= _mm_mullo_epi16(_mm_set1_epi16(2),l0);
+    	c0 = _mm_srli_si128(c0,4);
 
-    	c0= _mm_add_epi16(c0,c1);
-    	c2= _mm_add_epi16(c2,c3);
-    	c2= _mm_add_epi16(c2,add);
-    	c0= _mm_add_epi16(c0,c2);
+    	_mm_maskmoveu_si128(c0,mask,(__m128i*)(src + 2*stride)); //store only 4 values
 
-    	c0= _mm_srli_epi16(c0,3);
+    	c0 = _mm_srli_si128(c0,4);
 
-    	c0= _mm_packus_epi16(c0,_mm_setzero_si128());
+    	_mm_maskmoveu_si128(c0,mask,(__m128i*)(src + 3*stride)); //store only 4 values
 
-    	_mm_maskmoveu_si128(c0,mask,(__m128i*)(src+stride)); //store only 4 values
-
-    	ly1= _mm_set1_epi16(_mm_extract_epi16(ly,2));
-
-    	c0= _mm_mullo_epi16(tmp1,ly1);
-    	c1= _mm_mullo_epi16(tmp2,t0);
-    	c2= _mm_mullo_epi16(_mm_set1_epi16(1),tx);
-    	c3= _mm_mullo_epi16(_mm_set1_epi16(3),l0);
-
-    	c0= _mm_add_epi16(c0,c1);
-    	c2= _mm_add_epi16(c2,c3);
-    	c2= _mm_add_epi16(c2,add);
-    	c0= _mm_add_epi16(c0,c2);
-
-    	c0= _mm_srli_epi16(c0,3);
-
-    	c0= _mm_packus_epi16(c0,_mm_setzero_si128());
-
-    	_mm_maskmoveu_si128(c0,mask,(__m128i*)(src+2*stride)); //store only 4 values
-
-    	ly1= _mm_set1_epi16(_mm_extract_epi16(ly,3));
-
-    	c0= _mm_mullo_epi16(tmp1,ly1);
-    	c1= _mm_mullo_epi16(tmp2,t0);
-    	c2= _mm_mullo_epi16(_mm_setzero_si128(),tx);
-    	c3= _mm_mullo_epi16(_mm_set1_epi16(4),l0);
-
-    	c0= _mm_add_epi16(c0,c1);
-    	c2= _mm_add_epi16(c2,c3);
-    	c2= _mm_add_epi16(c2,add);
-    	c0= _mm_add_epi16(c0,c2);
-
-    	c0= _mm_srli_epi16(c0,3);
-
-    	c0= _mm_packus_epi16(c0,_mm_setzero_si128());
-
-    	_mm_maskmoveu_si128(c0,mask,(__m128i*)(src+3*stride)); //store only 4 values
 
 
 }
