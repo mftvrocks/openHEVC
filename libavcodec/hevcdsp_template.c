2075ecd2f92d377beb830a759b1273bf9087cedd
plepere <pierre-edouard.lepere@insa-rennes.fr>
2013-07-08 14:42:20 +0200

    upscaling optimisation

--- libavcodec/hevcdsp_template.c
+++ libavcodec/hevcdsp_template.c
@@ -5762,6 +5762,8 @@ for( j = 0; j < heightBL ; j++ )	{
             dstY++;
         }
         r15= _mm_set1_epi32(iOffset);
+    	r8= _mm_loadu_si128((__m128i*)coeff); //32 bit data, need 2 full loads to get all 8
+    	r9= _mm_loadu_si128((__m128i*)(coeff+4));
     	for( ; i <= (rightEndL-20); i+=8 )	{
         	r0= _mm_loadu_si128((__m128i*)srcY1);
         	r1= _mm_loadu_si128((__m128i*)(srcY1+widthEL));
@@ -5771,8 +5773,7 @@ for( j = 0; j < heightBL ; j++ )	{
         	r5= _mm_loadu_si128((__m128i*)(srcY1+widthEL*5));
         	r6= _mm_loadu_si128((__m128i*)(srcY1+widthEL*6));
         	r7= _mm_loadu_si128((__m128i*)(srcY1+widthEL*7));
-        	r8= _mm_loadu_si128((__m128i*)coeff); //32 bit data, need 2 full loads to get all 8
-        	r9= _mm_loadu_si128((__m128i*)(coeff+4));
+
       //  	printf("coeff value : %d vs %d\n", _mm_extract_epi32(r8,0), coeff[0]);
 
         	r11= _mm_shuffle_epi32(r8,_MM_SHUFFLE(0,0,0,0));
@@ -6031,20 +6032,142 @@ for( j = 0; j < heightBL ; j++ )	{
                         		}
                         		dstU++;
                         		dstV++;
-                                }else
-            for( i = 0; i < widthEL; i++ )	{
-                            		*dstU = av_clip_pixel( (CroVer_FILTER1(srcU1, coeff, widthEL) + iOffset) >> (nShift));
+            }else{
+
+            	for( ; i < widthEL; i++ )	{
+
+            	            		*dstU = av_clip_pixel( (CroVer_FILTER1(srcU1, coeff, widthEL) + iOffset) >> (nShift));
 
 
-                            		*dstV = av_clip_pixel( (CroVer_FILTER1(srcV1, coeff, widthEL) + iOffset) >> (nShift));
+            	            		*dstV = av_clip_pixel( (CroVer_FILTER1(srcV1, coeff, widthEL) + iOffset) >> (nShift));
 
-                        		if( (i >= leftStartC) && (i <= rightEndC-2) )	{
-                        			srcU1++;
-                                    srcV1++;
-                        		}
-                        		dstU++;
-                        		dstV++;
-                                }
+            	            		if( (i >= leftStartC) && (i <= rightEndC-2) )	{
+            	            			srcU1++;
+            	            			srcV1++;
+            	            		}
+            	            		dstU++;
+            	            		dstV++;
+            	            	}
+            	//R8 and R9 are coeffs
+            	//r15 is iOffset on 32bit
+            	//r11= _mm_packs_epi32(r8,r8); // only need 4 first coeffs
+            	for( i = 0; i < widthEL-7; i++ )	{
+            /*		r0= _mm_loadu_si128((__m128i*)srcU1);
+            		r2= _mm_loadu_si128((__m128i*)srcU1);
+            		r4= _mm_loadu_si128((__m128i*)srcU1);
+            		r6= _mm_loadu_si128((__m128i*)srcU1);
+
+            		//r1= _mm_loadu_si128((__m128i*)srcV1);
+
+            		r1= _mm_unpackhi_epi8(r0,_mm_setzero_si128());
+            		r0= _mm_unpacklo_epi8(r0,_mm_setzero_si128());	*/
+
+               /* 	r0= _mm_loadu_si128((__m128i*)srcU1);
+                	r1= _mm_loadu_si128((__m128i*)(srcU1+widthEL));
+                	r2= _mm_loadu_si128((__m128i*)(srcU1+widthEL*2));
+                	r3= _mm_loadu_si128((__m128i*)(srcU1+widthEL*3));
+
+
+              //  	printf("coeff value : %d vs %d\n", _mm_extract_epi32(r8,0), coeff[0]);
+
+                	r11= _mm_shuffle_epi32(r8,_MM_SHUFFLE(0,0,0,0));
+                	r11= _mm_packs_epi32(r11,r11);
+                	r14= _mm_mullo_epi16(r11,r0);
+                	r10= _mm_mulhi_epi16(r11,r0);
+                	r12= _mm_unpacklo_epi16(r14,r10);
+                	r13= _mm_unpackhi_epi16(r14,r10);
+             //   	printf("0 : %d vs %d\n",_mm_extract_epi32(r12,0),srcY1[0]*coeff[0]);
+
+                	r11= _mm_shuffle_epi32(r8,_MM_SHUFFLE(1,1,1,1));
+                	r11= _mm_packs_epi32(r11,r11);
+                	r0= _mm_mullo_epi16(r11,r1);
+                	r10= _mm_mulhi_epi16(r11,r1);
+                	r12= _mm_add_epi32(r12,_mm_unpacklo_epi16(r0,r10));
+                	r13= _mm_add_epi32(r13,_mm_unpackhi_epi16(r0,r10));
+            //   	printf("1 : %d vs %d\n",_mm_extract_epi32(r12,0),srcY1[0]*coeff[0] + srcY1[widthEL]*coeff[1]);
+
+
+                	r11= _mm_shuffle_epi32(r8,_MM_SHUFFLE(2,2,2,2));
+                	r11= _mm_packs_epi32(r11,r11);
+                	r0= _mm_mullo_epi16(r11,r2);
+                	r10= _mm_mulhi_epi16(r11,r2);
+                	r12= _mm_add_epi32(r12,_mm_unpacklo_epi16(r0,r10));
+                	r13= _mm_add_epi32(r13,_mm_unpackhi_epi16(r0,r10));
+           //     	printf("2 : %d vs %d\n",_mm_extract_epi32(r13,0),srcY1[4]*coeff[0] + srcY1[widthEL+4]*coeff[1] + srcY1[widthEL*2+4]*coeff[2]);
+
+
+                	r11= _mm_shuffle_epi32(r8,_MM_SHUFFLE(3,3,3,3));
+                	r11= _mm_packs_epi32(r11,r11);
+                	//printf("coeff value : %d vs %d\n", _mm_extract_epi16(r11,0), coeff[3]);
+                	r0= _mm_mullo_epi16(r11,r3);
+                	r10= _mm_mulhi_epi16(r11,r3);
+                	r12= _mm_add_epi32(r12,_mm_unpacklo_epi16(r0,r10));
+                	r13= _mm_add_epi32(r13,_mm_unpackhi_epi16(r0,r10));
+               // 	printf("3 : %d vs %d\n",_mm_extract_epi32(r12,0),srcY1[0]*coeff[0] + srcY1[widthEL]*coeff[1] + srcY1[widthEL*2]*coeff[2]+ srcY1[widthEL*3]*coeff[3]);
+
+                	//srcY1+=4;
+              //  	printf("coeff add : %d vs %d\n",_mm_extract_epi32(r12,0),LumVer_FILTER1(srcY1, coeff, widthEL));
+                	//srcY1-=4;
+
+                	r0= _mm_add_epi32(r12,r15);
+                	r10= _mm_add_epi32(r13,r15);
+              //  	printf("coeff ioff : %d vs %d\n",_mm_extract_epi32(r0,0),LumVer_FILTER1(srcY1, coeff, widthEL)+iOffset);
+
+                	r0= _mm_srai_epi32(r0,nShift);
+                	r10= _mm_srai_epi32(r10,nShift);
+            //    	printf("coeff shift : %d vs %d\n",_mm_extract_epi32(r0,0),(LumVer_FILTER1(srcY1, coeff, widthEL)+iOffset)>>nShift);
+
+                	r0= _mm_packus_epi32(r0,r10);
+                	r0= _mm_packus_epi16(r0,_mm_setzero_si128());
+
+                	_mm_storel_epi64((__m128i*)dstY,r0);*/
+
+
+            		*dstU = av_clip_pixel( (CroVer_FILTER1(srcU1, coeff, widthEL) + iOffset) >> (nShift));
+
+
+            		*dstV = av_clip_pixel( (CroVer_FILTER1(srcV1, coeff, widthEL) + iOffset) >> (nShift));
+
+            		if( (i >= leftStartC) && (i <= rightEndC-2) )	{
+            			srcU1++;
+            			srcV1++;
+            		}
+            		dstU++;
+            		dstV++;
+            	}
+
+            	for( ; i < widthEL; i++ )	{
+
+            		*dstU = av_clip_pixel( (CroVer_FILTER1(srcU1, coeff, widthEL) + iOffset) >> (nShift));
+
+
+            		*dstV = av_clip_pixel( (CroVer_FILTER1(srcV1, coeff, widthEL) + iOffset) >> (nShift));
+
+            		if( (i >= leftStartC) && (i <= rightEndC-2) )	{
+            			srcU1++;
+            			srcV1++;
+            		}
+            		dstU++;
+            		dstV++;
+            	}
+            	for( ; i < widthEL; i++ )	{
+
+            	            		*dstU = av_clip_pixel( (CroVer_FILTER1(srcU1, coeff, widthEL) + iOffset) >> (nShift));
+
+
+            	            		*dstV = av_clip_pixel( (CroVer_FILTER1(srcV1, coeff, widthEL) + iOffset) >> (nShift));
+
+            	            		if( (i >= leftStartC) && (i <= rightEndC-2) )	{
+            	            			srcU1++;
+            	            			srcV1++;
+            	            		}
+            	            		dstU++;
+            	            		dstV++;
+            	            	}
+
+
+            }
+
             }
 }
 
